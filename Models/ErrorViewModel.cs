78a07809d6bfae2626e4a04a843d1c96e3f1f406
André Akira Kusaba <43036207856@info.local>
2019-11-08 15:31:22 -0200

    SENAI - 07/11

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC___RoleTop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
