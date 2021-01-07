1fb8aff70f3a5f8d38ade0315637a0e8b1a5aed2
DevAndreAkira <pianista.akira@gmail.com>
2021-01-06 23:13:49 -0300

    Resolvendo problema de fechamendo de chave

--- js/script.js
+++ js/script.js
@@ -17,6 +17,8 @@ btnfechar.onclick = function () {
   site1.classList.add("selecao");
   site2.classList.remove("selecaoOn");
   site2.classList.add("selecao");
+  site3.classList.remove("selecaoOn");
+  site3.classList.add("selecao");
 
   document.getElementById("a3tech").classList.add("none");
   document.getElementById("labufalina").classList.add("none");
@@ -34,7 +36,6 @@ site1.onclick = function () {
     site3.classList.add("selecao");
     document.getElementById("a3tech").classList.remove("none");
     document.getElementById("white").classList.add("none");
-    document.getElementById("senaitechvagas").classList.add("none");
   }
   else if (site1.classList == "selecao" && site2.classList == "selecaoOn" && site3.classList == "selecao") {
     site1.classList.remove("selecao");
@@ -78,7 +79,6 @@ site2.onclick = function () {
     site1.classList.add("selecao");
     document.getElementById("labufalina").classList.remove("none");
     document.getElementById("white").classList.add("none");
-    document.getElementById("senaitechvagas").classList.add("none");
   }
   else if (site1.classList == "selecao" && site2.classList == "selecaoOn" && site3.classList == "selecao") {
     site2.classList.remove("selecao");
@@ -114,13 +114,12 @@ site2.onclick = function () {
   else {
 
   }
+}
 
-  site3.onclick = function () {
-    if (site1.classList == "selecao" && site2.classList == "selecao" && site3.classList == "selecao") {
+  site3.onclick = function () {    
+    if (site1.classList == "selecao" && site2.classList == "selecao" && site3.classList == "selecao"){
       site3.classList.remove("selecao");
       site3.classList.add("selecaoOn");
-      site2.classList.remove("selecaoOn");
-      site2.classList.add("selecao");
       site1.classList.remove("selecaoOn");
       site1.classList.add("selecao");
       document.getElementById("senaitechvagas").classList.remove("none");
@@ -160,8 +159,6 @@ site2.onclick = function () {
       document.getElementById("senaitechvagas").classList.remove("none");
     }
     else {
-  
+
     }
   }
-
-}
