8c299146c0feb9f31063e2326a9a8fc8af44a9d7
DevAndreAkira <pianista.akira@gmail.com>
2021-01-07 21:06:41 -0300

    Adicionando trabalho - Joel Jota

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ var btnfechar = document.getElementById("fecharModal");
 var site1 = document.getElementById("site1");
 var site2 = document.getElementById("site2");
 var site3 = document.getElementById("site3");
+var site4 = document.getElementById("site4");
 
 btnProject.onclick = function () {
   modalproject.classList.remove('none');
@@ -19,10 +20,13 @@ btnfechar.onclick = function () {
   site2.classList.add("selecao");
   site3.classList.remove("selecaoOn");
   site3.classList.add("selecao");
+  site4.classList.remove("selecaoOn");
+  site4.classList.add("selecao");
 
   document.getElementById("a3tech").classList.add("none");
   document.getElementById("labufalina").classList.add("none");
   document.getElementById("senaitechvagas").classList.add("none");
+  document.getElementById("joel").classList.add("none");
   document.getElementById("white").classList.remove("none");
 }
 
@@ -30,37 +34,62 @@ site1.onclick = function () {
   site1.classList.remove("selecao");
   site2.classList.remove("selecaoOn");
   site3.classList.remove("selecaoOn");
+  site4.classList.remove("selecaoOn");
   site1.classList.add("selecaoOn");
   site2.classList.add("selecao");
   site3.classList.add("selecao");
+  site4.classList.add("selecao");
   document.getElementById("a3tech").classList.remove("none");
   document.getElementById("white").classList.add("none");
   document.getElementById("senaitechvagas").classList.add("none");
   document.getElementById("labufalina").classList.add("none");
+  document.getElementById("joel").classList.add("none");
 }
 
 site2.onclick = function () {
   site1.classList.remove("selecaoOn");
   site3.classList.remove("selecaoOn");
+  site4.classList.remove("selecaoOn");
   site2.classList.remove("selecao");
   site2.classList.add("selecaoOn");
   site1.classList.add("selecao");
+  site4.classList.add("selecao");
   site3.classList.add("selecao");
   document.getElementById("labufalina").classList.remove("none");
   document.getElementById("white").classList.add("none");
   document.getElementById("a3tech").classList.add("none");
   document.getElementById("senaitechvagas").classList.add("none");
+  document.getElementById("joel").classList.add("none");
 }
 
 site3.onclick = function () {
   site1.classList.remove("selecaoOn");
   site2.classList.remove("selecaoOn");
+  site4.classList.remove("selecaoOn");
   site3.classList.remove("selecao");
   site3.classList.add("selecaoOn");
   site1.classList.add("selecao");
   site2.classList.add("selecao");
+  site4.classList.add("selecao");
   document.getElementById("senaitechvagas").classList.remove("none");
   document.getElementById("white").classList.add("none");
   document.getElementById("a3tech").classList.add("none");
   document.getElementById("labufalina").classList.add("none");
+  document.getElementById("joel").classList.add("none");
+}
+
+site4.onclick = function () {
+  site1.classList.remove("selecaoOn");
+  site2.classList.remove("selecaoOn");
+  site3.classList.remove("selecaoOn");
+  site4.classList.remove("selecao");
+  site4.classList.add("selecaoOn");
+  site1.classList.add("selecao");
+  site2.classList.add("selecao");
+  site3.classList.add("selecao");
+  document.getElementById("joel").classList.remove("none");
+  document.getElementById("white").classList.add("none");
+  document.getElementById("a3tech").classList.add("none");
+  document.getElementById("labufalina").classList.add("none");
+  document.getElementById("senaitechvagas").classList.add("none");
 }
